7a0a12fb2e98a516d10288b838634e82470cf6e4
Utkarsh Verma <uv5088@srmist.edu.in>
2025-06-24 15:33:41 +0530

    revision

--- DSA - C++/Practice Questions/Important Questions/tempCodeRunnerFile.cpp
+++ DSA - C++/Practice Questions/Important Questions/tempCodeRunnerFile.cpp
@@ -1,36 +1,7 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
 using namespace std;
 
-vector<int> sort01(vector<int> &v) {
-    int i = 0; int j = v.size() - 1;
-    while(i < j) {
-        while(v[i] == 0 & i < j) {
-            i++;
-        }
-        while(v[j] == 1 & i < j) {
-            j--;
-        }
-        if(i < j) {
-            swap(v[i], v[j]);
-            i++; j--;
-        }
-    }
-}
-
 int main() {
-    int n;
-    cin>>n;
-    vector<int> v(n);
-    for(int i = 0; i < n; i++) {
-        cin>>v[i];
-        if(v[i] != 0 && v[i] != 1) {
-            cout<<"Restart the program and enter value 0/1";
-            exit(0);
-        }
-    }
-    sort01(v);
-    for(int i = 0; i< v.size(); i++) {
-        cout<<v[i]<<" ";
-    }
+    cout << "__cplusplus: " << __cplusplus << endl;
+    return 0;
 }
