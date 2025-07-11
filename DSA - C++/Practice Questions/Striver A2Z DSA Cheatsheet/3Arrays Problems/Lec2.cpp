88d27b31be306fde19b7fcdf27da905f48daee5a
Utkarsh Verma <uv5088@srmist.edu.in>
2025-07-11 18:53:49 +0530

    revision and practice

--- DSA - C++/Practice Questions/Striver A2Z DSA Cheatsheet/3Arrays Problems/Lec2.cpp
+++ DSA - C++/Practice Questions/Striver A2Z DSA Cheatsheet/3Arrays Problems/Lec2.cpp
@@ -63,7 +63,7 @@ bool twoSum2_V1(vector<int> v, int k) {
     return false;
 }
 
-bool twoSum3_V1(vector<int> v, int k) {
+bool twoSum3_V1(vector<int> v, int k) { // Faster due to unordered_map!
     int n = v.size();
     unordered_map<int, int> mp;
     for(int i = 0; i < n; i++) {
@@ -207,11 +207,8 @@ using namespace std;
 pair<int, int> twoSum2_V2(vector<int> v, int k) {
     unordered_map<int, int> seen;
     for(int i = 0; i < v.size(); i++) {
-        int val = v[i];
-        if(seen.find(k - val) != seen.end()) {
-            return {seen[k - val], i};
-        }
-        seen[val] = i;
+        if(seen.find(k - v[i]) != seen.end()) return {seen[k - v[i]], i};
+        seen[v[i]] = i;
     }
     return {-1, -1};
 }
@@ -264,4 +261,155 @@ int main() {
     cin>>k;
     pair<int, int> ans = twoSum2_V2(v, k);
     cout<<"["<<ans.first<<", "<<ans.second<<"]"<<endl;
+}
+
+// Question 2 : Sort 0s, 1s, 2s!
+//            : Given an array consisting of only 0s, 1s, and 2s. Write a program to in-place sort the array without using inbuilt sort functions. ( Expected: Single pass-O(N) and constant space)
+// Approach 1 (Brute Force) : Simply use any sorting algorithm! But those algorithms we know as of now are either slow or takes an extra space!
+// Approach 2 (Dutch National Flag Algorithm) : It’s called the Dutch National Flag algorithm because : The Dutch flag has three bands: red, white, blue. Similarly, we want to sort 3 types of elements into 3 separate "bands" or regions.
+//                                            : In this classic problem : 0 → First color (e.g., red). 1 → Middle color (e.g., white). 2 → Last color (e.g., blue)
+//                                            : We maintain 3 regions in the array using 3 pointers : low  |    Left region for 0s    | All elements left of low are 0s
+//                                                                                                  : mid  |  Current element pointer | To check and decide where current element goes
+//                                                                                                  : high |    Right region for 2s   | All elements right of high are 2s
+//                                            : We process the array from left to right, and at each step : If arr[mid] == 0 : This belongs on the left side, Swap with arr[low], and move low and mid forward, Because you’ve handled one more 0
+//                                                                                                        : If arr[mid] == 1 : This is already in the middle zone, Just move mid++
+//                                                                                                        : If arr[mid] == 2 : This belongs on the right side, Swap with arr[high], and only move high--. mid is not moved, because the new value at mid still needs to be processed.
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void sort012(vector<int> &v) {
+    int low = 0;
+    int mid = 0;
+    int high = v.size() - 1;
+    while(mid <= high) {
+        if(v[mid] == 0) {
+            swap(v[low], v[mid]);
+            mid++; low++;
+        }
+        else if(v[mid] == 1) {
+            mid++;
+        }
+        else if(v[mid] == 2) {
+            swap(v[high], v[mid]);
+            high--;
+        }
+    }
+}
+
+int main() {
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for(int i = 0; i < n; i++) {
+        cin>>v[i];
+    }
+    sort012(v);
+    for(int i = 0; i < n; i++) {
+        cout<<v[i]<<" ";
+    }
+}
+
+// Question 3 : Find the Majority Element that occurs more than N/2 times!
+//            : Given an array of N integers, write a program to return an element that occurs more than N/2 times in the given array. You may consider that such an element always exists in the array.
+// Approach 1 (Brute Force) : Iterate in a nested loop and count occurences! and if it gets > n/2, then it is majority element! TC : O(n^2) & SC : O(1)
+// Approach 2 (Maps) : Iterate the arrays once, and store count of every element! and if it gets > n/2, then it is majority element! TC : O(n logn) & SC : O(n)
+// Approach 2.2 (Unordered Maps) : Iterate the arrays once, and store count of every element! and if it gets > n/2, then it is majority element! TC : O(n) for avg & best case & SC : O(n)
+// Approach 3 (Moore's Voting Algorithm) : Imagine you're in a room full of people, and you're conducting a vote to decide who the most popular person is.
+//                                       : Let’s say : Every person either votes for a candidate (say A) or votes against them. When someone supports A, A gains +1. When someone opposes A, it’s like A loses -1.
+//                                       : If you keep doing this : If A is truly supported by more than half, then after all ups and downs, they'll still stand strong. Anyone who isn’t backed by a majority will get “eliminated” over time.
+//                                       : This is exactly what Moore’s Voting Algorithm simulates!
+//                                       : Phase 1 : Finding a Candidate : We use count = 0 & element = -1 (or any default)
+//                                                 : What is this doing? : If count == 0: we "reset" our candidate — like electing a new leader.
+//                                                                       : If current value == current candidate → support received → count++
+//                                                                       : If different value → opposition → count--
+//                                                                       : So candidate’s strength goes up or down, depending on match/mismatch
+//                                                 : But wait — what if this candidate is not majority? That's why we have...
+//                                       : Phase 2 : Verifying the Candidate : We now count how many times the candidate actually appears in the array
+//                                                 : Why This Works : Let’s assume that there is a majority element.
+//                                                                  : Let’s say M is that element, and it appears more than n/2 times.
+//                                                                  : Every time we see M, count++
+//                                                                  : Every time we see a non-M, count--
+//                                                                  : Since M appears more than the rest combined, the net count will be positive and M will survive the elimination battle.
+//                                                                  : Hence, at the end of Phase 1, we are guaranteed to get the majority element (if it exists). If it doesn’t exist, Phase 1 may still give a candidate, but Phase 2 will catch the mistake.
+//                                       : Time Complexity : O(n) & Space Complexity : O(1)
+#include<iostream>
+using namespace std;
+
+void findMajorityElement(int* arr, int n) {
+    int count = 0;
+    int element = 0;
+    for(int i = 0; i < n; i++) {
+        if(count == 0) {
+            count = 1;
+            element = arr[i];
+        }
+        else if(arr[i] == element) {
+            count++;
+        }
+        else {
+            count--;
+        }
+    }
+    int countFinal = 0;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == element) {
+            countFinal++;
+        }
+    }
+    (countFinal > n/2) ? cout << element : cout << "No Majority Element!";
+}
+
+int main() {
+    int n;
+    cin>>n;
+    int* arr = (int*)malloc(n * sizeof(int));
+    // int* arr = new int[n]; // Modern and correct in C++
+    // vector<int> arr(n); // Best for simplicity
+    for(int i = 0; i < n; i++) {
+        cin>>arr[i];
+    }
+    findMajorityElement(arr, n);
+}
+
+// Question 3 : Maximum Subarray Sum in an Array (Kadane's Algorithm)
+//            : Given an integer array arr, find the contiguous subarray (containing at least one number) which has the largest sum and returns its sum and prints the subarray.
+// Approach 1 (Brute Force) : Iterate over the nested loop! and simply update maxSum & start & end variables! and lastly print the subarray and return the maxSum!
+// Approach 2 (Kadane's Algorithm) : 
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int maxSumSubarray(int* arr, int n) {
+    int maxSum = INT_MIN;
+    int start = 0;
+    int end = 0;
+    for(int i = 0; i < n; i++) {
+        int sum = 0;
+        for(int j = i; j < n; j++) {
+            sum += arr[j];
+            if(sum > maxSum) {
+                maxSum= sum;
+                start = i;
+                end = j;
+            }
+        }
+    }
+    for(int i = start; i <= end; i++) {
+        cout<<arr[i]<<" ";
+    }
+    return maxSum;
+}
+
+int main() {
+    int n;
+    cin>>n;
+    int* arr = new int[n]; // Modern and correct in C++
+    // vector<int> arr(n); // Best for simplicity
+    for(int i = 0; i < n; i++) {
+        cin>>arr[i];
+    }
+    int ans = maxSumSubarray(arr, n);
+    cout<<ans;
+    delete[] arr;
 }
--- DSA - C++/Practice Questions/Striver A2Z DSA Cheatsheet/3Arrays Problems/tempCodeRunnerFile.cpp
+++ DSA - C++/Practice Questions/Striver A2Z DSA Cheatsheet/3Arrays Problems/tempCodeRunnerFile.cpp
@@ -1,28 +1,37 @@
 #include<iostream>
-#include<vector>
+#include<climits>
 using namespace std;
 
-pair<int, int> twoSum2_V2(vector<int> v, int k) {
-    int n = v.size();
-    int i = 0; int j = n - 1;
-    while(i < j) {
-        int sum = v[i] + v[j];
-        if(sum == k) return {i, j};
-        if(sum > k) j--;
-        else i++;
+int maxSumSubarray(int* arr, int n) {
+    int maxSum = INT_MIN;
+    int start = 0;
+    int end = 0;
+    for(int i = 0; i < n; i++) {
+        int sum = 0;
+        for(int j = i; j < n; j++) {
+            sum += arr[j];
+            if(sum > maxSum) {
+                maxSum= sum;
+                start = i;
+                end = j;
+            }
+        }
     }
-    return {-1, -1};
+    for(int i = start; i <= end; i++) {
+        cout<<arr[i]<<" ";
+    }
+    return maxSum;
 }
 
 int main() {
     int n;
     cin>>n;
-    vector<int> v(n);
+    int* arr = new int[n]; // Modern and correct in C++
+    // vector<int> arr(n); // Best for simplicity
     for(int i = 0; i < n; i++) {
-        cin>>v[i];
+        cin>>arr[i];
     }
-    int k;
-    cin>>k;
-    pair<int, int> ans = twoSum2_V2(v, k);
-    cout<<"["<<ans.first<<", "<<ans.second<<"]"<<endl;
+    int ans = maxSumSubarray(arr, n);
+    cout<<ans;
+    delete[] arr;
 }
