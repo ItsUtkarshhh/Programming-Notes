3f391c1ef90495fd881752af4777aed95fc95349
Utkarsh Verma <u.verma1402@gmail.com>
2025-12-01 00:10:57 +0530

    Practice

--- C++ DSA/Practice Questions/Lecture Wise Questions/tempCodeRunnerFile.cpp
+++ C++ DSA/Practice Questions/Lecture Wise Questions/tempCodeRunnerFile.cpp
@@ -1,30 +1,66 @@
 #include<iostream>
-#include<vector>
 #include<map>
+#include<set>
+#include<vector>
 using namespace std;
 
-int findKeyLS3(vector<int> v, int key) {
-    map<int, int> mp;
-    for(int i = 0; i < v.size(); i++) {
-        if(!mp.count(v[i])) {
-            mp[v[i]] = i;
+vector<vector<int>> findPairSum3(vector<int> v, int sum) {
+    map<int, int> freq;
+    set<pair<int, int>> valPair;
+
+    for (int num : v) {
+        freq[num]++;
+    }
+
+    for (int i = 0; i < v.size(); i++) {
+        int complement = sum - v[i];
+
+        // Case 1: Pair of same elements (like 2 + 2 = 4)
+        if (v[i] == complement && freq[v[i]] >= 2) {
+            valPair.insert({v[i], v[i]});
+            freq[v[i]] = 0; // mark both used
+        }
+
+        // Case 2: Normal case with two different values
+        else if (v[i] != complement && freq[v[i]] > 0 && freq[complement] > 0) { // You may remove : v[i] != complement
+            valPair.insert({min(v[i], complement), max(v[i], complement)});
+            freq[v[i]] = 0;
+            freq[complement] = 0;
         }
     }
-    if(!mp.count(key)) return -1;
-    return mp[key];
+
+    vector<vector<int>> result;
+    for (auto &i : valPair) {
+        result.push_back({i.first, i.second});
+    }
+
+    return result.size() > 0 ? result : vector<vector<int>>{{-1}};
 }
 
 int main() {
     int n;
     cin>>n;
     vector<int> v(n);
-
     for(int i = 0; i < n; i++) {
         cin>>v[i];
     }
-
-    int key;
-    cin>>key;
-
-    cout<<"Index of the key (if exist) : "<<findKeyLS3(v, key);
+    int sum;
+    cin>>sum;
+    vector<vector<int>> ans = findPairSum3(v, sum);
+    if (ans.size() == 1 && ans[0].size() == 1 && ans[0][0] == -1) {
+        cout << "[-1]";
+    }
+    else {
+        cout << "[";
+        for (int i = 0; i < ans.size(); i++) {
+            cout << "[";
+            for (int j = 0; j < ans[i].size(); j++) {
+                cout << ans[i][j];
+                if (j != ans[i].size() - 1) cout << ", ";
+            }
+            cout << "]";
+            if (i != ans.size() - 1) cout << ", ";
+        }
+        cout << "]";
+    }
 }
--- C++ DSA/Practice Questions/RandomPracticeSpace.cpp
+++ C++ DSA/Practice Questions/RandomPracticeSpace.cpp
@@ -1,40 +1,66 @@
 #include<iostream>
 #include<vector>
+#include<map>
 using namespace std;
 
-int findKey(vector<int> v, int key) {
-    int start = 0;
-    int end = v.size() - 1;
-    int mid = start + (end - start)/2;
-    while(start <= end) {
-        if(key == v[mid]) {
-            return mid;
-        }
-
-        if(key > v[mid]) {
-            start = mid + 1;
-        }
-        else {
-            end = mid - 1;
-        }
-
-        mid = start + (end - start)/2;
+void sort01(vector<int> &v) {
+    map<int, int> mp;
+    for(int i = 0; i < v.size(); i++) {
+        mp[v[i]]++;
     }
 
-    return -1;
+    int i = 0;
+    while(mp[0]) {
+        v[i] = 0;
+        i++;
+        mp[0]--;
+    }
+
+    while(mp[1]) {
+        v[i] = 1;
+        i++;
+        mp[1]--;
+    }
+}
+
+void sort01_2(vector<int> &v) {
+    int i = 0;
+    int j = v.size() - 1;
+
+    while(i < j) {
+        if(v[i] == 1 && v[j] == 0) {
+            swap(v[i], v[j]);
+            i++;
+            j--;
+        }
+
+        else if(v[i] == 0 && v[j] == 0) {
+            i++;
+        }
+
+        else if(v[i] == 1 && v[j] == 1) {
+            j--;
+        }
+
+        else {
+            i++;
+            j--;
+        }
+    }
 }
 
 int main() {
     int n;
     cin>>n;
-    vector<int> v(n);
 
+    vector<int> v(n);
     for(int i = 0; i < n; i++) {
         cin>>v[i];
     }
 
-    int key;
-    cin>>key;
+    sort01_2(v);
 
-    cout<<"Value is at index : "<<findKey(v, key);
+    for(int i = 0; i < v.size(); i++) {
+        cout<<v[i]<<" ";
+    }
 }
