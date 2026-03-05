4c505daa5a3c74d963767b8b51aed67e4b7aa679
Utkarsh Verma <u.verma1402@gmail.com>
2026-03-06 00:17:56 +0530

    Practice

--- C++ DSA/Practice Questions/RandomPracticeSpace.cpp
+++ C++ DSA/Practice Questions/RandomPracticeSpace.cpp
@@ -7,12 +7,12 @@ class Node {
     public:
     int data;
     Node* next;
-    Node* random;
+    Node* prev;
 
     Node(int data) {
         this->data = data;
         this->next = NULL;
-        this->random = NULL;
+        this->prev = NULL;
     }
 };
 
@@ -23,188 +23,54 @@ void insertNode(Node* &head, Node* &tail, int data) {
         return;
     }
     tail->next = newNode;
+    newNode->prev = tail;
     tail = newNode;
 }
 
-Node* revLL(Node* head) {
+void printLL(Node* &head) {
+    if(head == NULL) return;
+
+    Node* temp = head;
+    while(temp != NULL) {
+        cout<<temp->next<<" > ";
+        temp = temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+
+// Delete all occurences of a key from the in DLL!
+Node* deleteAllOcc(Node* head, int data) {
     if(head == NULL) return NULL;
 
-    Node* prev = NULL;
+    Node* dummyNode = new Node(INT_MIN);
+    Node* prev = dummyNode;
     Node* curr = head;
     Node* forward = NULL;
 
     while(curr != NULL) {
         forward = curr->next;
-        curr->next = prev;
+        if(curr->data == data) {
+            Node* ntd = curr;
+            prev->next = forward;
+            forward->prev = prev;
+            ntd->next = NULL;
+            ntd->prev = NULL;
+            delete ntd;
+        }
         prev = curr;
         curr = forward;
     }
-    return prev;
-}
-
-Node* cloneRandom(Node* head) {
-    if(head == NULL) return NULL;
-
-    Node* originalHead = head;
-    Node* cloneHead = NULL;
-    Node* cloneTail = NULL;
-
-    while(originalHead != NULL) {
-        Node* newNode = new Node(originalHead->data);
-        if(cloneHead == NULL || cloneTail == NULL) cloneHead = cloneTail = newNode;
-        else {
-            cloneTail->next = newNode;
-            cloneTail = newNode;
-        }
-        originalHead = originalHead->next;
-    }
-    originalHead = head;
-
-    Node* original = head;
-    Node* clone = cloneHead;
-
-    while(original != NULL) {
-        if(original->random != NULL) {
-            Node* tempOrg = head;
-            Node* tempClone = cloneHead;
-            while(tempOrg != NULL) {
-                if(original->random == tempOrg) {
-                    clone->random = tempClone;
-                    break;
-                }
-                tempOrg = tempOrg->next;
-                tempClone = tempClone->next;
-            }
-        }
-        original = original->next;
-        clone = clone->next;
-    }
-    return cloneHead;
-}
-
-Node* cloneRandom(Node* head) {
-    if(head == NULL) return NULL;
-
-    Node* tempOrg = head;
-    Node* cloneHead = NULL;
-    Node* cloneTail = NULL;
-    unordered_map<Node*, Node*> oldToNew;
-
-    while(tempOrg != NULL) {
-        Node* newNode = new Node(tempOrg->data);
-        if(cloneHead == NULL || cloneTail == NULL) {
-            cloneHead = cloneTail = newNode;
-        }
-        else {
-            cloneTail->next = newNode;
-            cloneTail = newNode;
-        }
-        oldToNew[tempOrg] = cloneTail;
-        tempOrg = tempOrg->next;
-    }
-
-    tempOrg = head;
-    Node* tempClone = cloneHead;
-    while(tempOrg != NULL) {
-        tempClone->random = oldToNew[tempOrg->random]
-        tempOrg = tempOrg->next;
-        tempClone = tempClone->next;
-    }
-
-    return cloneHead;
-}
-
-Node* cloneRandom3(Node* head) {
-    if(head == NULL) return NULL;
-
-    Node* tempOrg = head;
-    while(tempOrg != NULL) {
-        Node* newNode = new Node(tempOrg->data);
-        newNode->next = tempOrg->next;
-        tempOrg->next = newNode;
-        tempOrg = newNode->next;
-    }
-
-    tempOrg = head;
-    while(tempOrg != NULL) {
-        if(tempOrg->random != NULL) {
-            tempOrg->next->random = tempOrg->random->next;
-        }
-        tempOrg = tempOrg->next->next;
-    }
-
-    tempOrg = head;
-    Node* cloneHead = head->next;
-    Node* tempClone = cloneHead;
-    while(tempClone != NULL) {
-        tempOrg->next = tempOrg->next->next;
-        if(tempClone->next != NULL) tempClone->next = tempClone->next->next;
-        tempOrg = tempOrg->next;
-        tempClone = tempClone->next;
-    }
-    return cloneHead;
-}
-
-NodE* mergeLL(Node* LL1, Node* LL2) {
-    if(LL1 == NULL) return LL2;
-    if(LL2 == NULL) return LL1;
-
-    Node* dummyHead = new Node(INT_MIN);
-    Node* temp = dummyHead;
-
-    while(LL1 != NULL && LL2 != NULL) {
-        if(LL1->data <= LL2->data) {
-            temp->down = LL1;
-            temp = LL1;
-            LL1 = LL1->down;
-        }
-        else {
-            temp->down = LL2;
-            temp = LL2;
-            LL2 = LL2->down;
-        }
-        temp->next = NULL;
-    }
-
-    if(LL1 != NULL) temp->down = LL1;
-    if(LL2 != NULL) temp->down = LL2;
-
-    return dummyHead->down;
-}
-
-Node* flattenLL(Node* head) {
-    if(head == NULL || head->next == NULL) return head;
-
-    Node* mergeHead = flattenLL(head->next);
-    return mergeLL(head, mergeHead);
-}
-
-void printList(Node* head) {
-    if(head == NULL) return;
-    Node* temp = head;
-
-    while(temp != NULL) {
-        cout<<temp->data<<" ";
-        temp = temp->next;
-    }
+    return dummyNode->next;
 }
 
 int main() {
-    Node* n1 = new Node(1);
-    Node* head = n1;
-    Node* tail = n1;
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    insertNode(head, tail, 1);
+    insertNode(head, tail, 2);
+    insertNode(head, tail, 3);
+    insertNode(head, tail, 4);
     insertNode(head, tail, 5);
-    insertNode(head, tail, 8);
-    insertNode(head, tail, 9);
-    insertNode(head, tail, 10);
-
-    Node* n2 = new Node(2);
-    Node* head2 = n2;
-    Node* tail2 = n2;
-
-    insertNode(head2, tail2, 5);
-    insertNode(head2, tail2, 6);
-
-    Node* ans = addingLLs(head, head2);
-    printList(ans);
+    insertNode(head, tail, 6);
 }
