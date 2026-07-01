b80979723afd7d8a45ae68176ae7032250ce3074
Utkarsh Verma <u.verma1402@gmail.com>
2026-07-01 23:30:27 +0530

    Practice

--- C++ DSA/Practice Questions/Striver A2Z Sheet/Sorting/sorting.cpp
+++ C++ DSA/Practice Questions/Striver A2Z Sheet/Sorting/sorting.cpp
@@ -12,18 +12,58 @@ using namespace std;
 void bubbleSort(int* arr, int size) {
     if(size == 0 || size == 1) return;
 
-    for(int i = 0; i < size; i++) {
+    for(int i = 0; i < size - 1; i++) {
+        for(int j = 0; j < size - i - 1; j++) {
+            if(arr[j] >= arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
 
+void selectionSort(int* arr, int size) {
+    if(size == 0 || size == 1) return;
+
+    for(int i = 0; i < size - 1; i++) {
+        int minIndex = i;
+        for(int j = i; j < size; j++) {
+            if(arr[j] <= arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap(arr[minIndex], arr[i]);
+    }
+}
+
+void insertionSort(int* arr, int size) {
+    if(size == 0 || size == 1) return;
+
+    for(int i = 0; i < size - 1; i++) {
+        int currElement = arr[i + 1];
+        int j;
+        for(j = i + 1; j > 0; j--) {
+            if(arr[j - 1] > arr[j]) {
+                arr[j] = arr[j-1];
+            }
+            else {
+                break;
+            }
+        }
+        arr[j] = currElement;
     }
 }
 
 int main() {
     int size;
+    cin>>size;
     int* arr = new int[size];
 
     for(int i = 0; i < size; i++) {
         cin>>arr[i];
     }
     
-    bubbleSort(arr, size);
+    insertionSort(arr, size);
+    for(int i = 0; i < size; i++) {
+        cout<<arr[i]<<" ";
+    }
 }
