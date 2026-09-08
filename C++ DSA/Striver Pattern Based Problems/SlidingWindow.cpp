c6c2818ffdadc4026b53978b929d98d36d64106b
ItsUtkarshhh <u.verma1402@gmail.com>
2026-09-08 15:00:19 +0530

    Striving Day 3

--- C++ DSA/Striver Pattern Based Problems/SlidingWindow.cpp
+++ C++ DSA/Striver Pattern Based Problems/SlidingWindow.cpp
@@ -147,4 +147,371 @@ int maxPointsFromCards(vector<int> &v, int k) {
 }
 
 // ----------------------------------------------------------------------- Problem 4 ---------------------------------------------------------------------------------------------------------------->
-// Problem : 
+// Problem : Longest Substring Without Repeating Characters
+// Brute Force : Try to generate all the subarrays, and use a hashmap to track occurrence of all of them. And update the max length accordingly.
+// Optimal : Use the sliding window logic, to traverse the string in the form of a window of sub strings, and check for the dupllications within those sub strings using hashmap.
+
+// Brute Force :
+int longestSubStringNoRep(string &str) {
+    if(str.empty()) return 0;
+
+    string subStr = "";
+    int maxLen = INT_MIN;
+    
+    for(int i = 0; i < str.length(); i++) {
+        subStr = "";
+        map<char, int> hash;
+        for(int j = i; j < str.length(); j++) {
+            if(hash[str[j]] == 0) {
+                subStr = subStr + str[j];
+                maxLen = max(maxLen, j - i + 1);
+                hash[str[j]]++;
+            }
+            else {
+                break;
+            }
+        }
+    }
+    return maxLen;
+}
+
+// Optimal :
+int longestSubStringNoRep(string &str) {
+    if(str.empty()) return 0;
+    
+    string subStr = "";
+    int maxLen = INT_MIN;
+    map<char, int> hash;
+    
+    for(char ch = 'a'; ch <= 'z'; ch++) {
+        hash[ch] = -1;
+    }
+    
+    int left = 0; int right = 0;
+    while(right < str.length()) {
+        if(hash[str[right]] == -1 || hash[str[right]] < left) {
+            subStr = subStr + str[right];
+            maxLen = max(maxLen, right - left + 1);
+        }
+        else {
+            left = hash[str[right]] + 1;
+        }
+        hash[str[right]] = right;
+        right++;
+    }
+    
+    return maxLen;
+}
+
+// ----------------------------------------------------------------------- Problem 5 ---------------------------------------------------------------------------------------------------------------->
+// Problem : Max Consecutive Ones III - Longest Subarray with at most K zeroes.
+// Brute Force : Generate all the sub arrays and keep couting the zeroes, if the zeroes exceeds K value, then simply calculate the length of that sub array. And similarly move forward.
+// Optimal 1 : Expand right, count zeroes, if zeroes exceeds K, then shrink left until zeroes < k, and track the maximum valid window length.
+// Optimal 2 : Expand right, count zeroes, if zeroes exceeds K, then shrink left once, and update the max length only when it zeroes are within range.
+
+// Brute :
+int maxConsecutiveOnes(vector<int> &v, int k) {
+    if(v.empty()) return 0;
+    
+    int zeroes = 0;
+    int maxLen = 0;
+    
+    for(int i = 0; i < v.size(); i++) {
+        zeroes = 0;
+        for(int j = i; j < v.size(); j++) {
+            if(v[j] == 0) {
+                zeroes++;
+            }
+            if(zeroes <= k) {
+                maxLen = max(maxLen, j - i + 1);
+            }
+            else {
+                break;
+            }
+        }
+    }
+    return maxLen;
+}
+
+// Optimal 1 :
+int maxConsecutiveOnes(vector<int> &v, int k) {
+    if(v.empty()) return 0;
+    
+    int zeroes = 0;
+    int maxLen = 0;
+    
+    int left = 0; int right = 0;
+    while(right < v.size()) {
+        if(v[right] == 0) {
+            zeroes++;
+        }
+        if(zeroes <= k) {
+            maxLen = max(maxLen, right - left + 1);
+        }
+        else {
+            while(zeroes > k) {
+                if(v[left] == 0) zeroes--;
+                left++;
+            }
+        }
+        right++;
+    }
+}
+
+// Optimal 2 :
+int maxConsecutiveOnes(vector<int> &v, int k) {
+    if(v.empty()) return 0;
+    
+    int zeroes = 0;
+    int maxLen = 0;
+    
+    int left = 0; int right = 0;
+    while(right < v.size()) {
+        if(v[right] == 0) {
+            zeroes++;
+        }
+        if(zeroes > k) {
+            if(v[left] == 0) zeroes--;
+            left++;
+        }
+        if(zeroes <= k) {
+            maxLen = max(maxLen, right - left + 1);
+        }
+        right++;
+    }
+    return maxLen;
+}
+
+// ----------------------------------------------------------------------- Problem 6 ---------------------------------------------------------------------------------------------------------------->
+// Problem : Fruit Into Baskets - Maximum length of sub array with at most two types of numbers.
+// Brute Force : Generate all the subarray, collects unique fruit types using a set, stops when types exceed two, and updates the maximum length found.
+// Optimal 1 : Use a sliding window with left and right pointers, shrinks the window incrementally using a while loop whenever unique fruit types exceed two. And update the maxLen everytime after the inner while loop.
+// Optimal 2 : Use a sliding window with left and right pointers, shrinks the window once whenever unique fruit types exceed two. And update the maxLen only when the condition is valid.
+
+// Brute Force :
+int totalFruit(vector<int> &v) {
+    if(v.empty()) return 0;
+    
+    int maxLen = 0;
+    
+    for(int i = 0; i < v.size(); i++) {
+        set<int> fruits;
+        for(int j = i; j < v.size(); j++) {
+            fruits.insert(v[j]);
+            if(fruits.size() > 2) {
+                break;
+            }
+            maxLen = max(maxLen, j - i + 1);
+        }
+    }
+    return maxLen;
+}
+
+// Optimal 1 :
+int totalFruit(vector<int> &v) {
+    if(v.empty()) return 0;
+    
+    int maxLen = 0;
+    
+    map<int, int> fruits;
+    int left = 0; int right = 0;
+    while(right < v.size()) {
+        fruits[v[right]]++;
+        
+        while(fruits.size() > 2) {
+            fruits[v[left]]--;
+            if(fruits[v[left]] == 0) {
+                fruits.erase(v[left]);
+            }
+            left++;
+        }
+        maxLen = max(maxLen, right - left + 1);
+        right++;
+    }
+    return maxLen;
+}
+
+// Optimal 2 :
+int totalFruit(vector<int> &v) {
+    if(v.empty()) return 0;
+    
+    int maxLen = 0;
+    
+    map<int, int> fruits;
+    int left = 0; int right = 0;
+    while(right < v.size()) {
+        fruits[v[right]]++;
+
+        if(fruits.size() > 2) {
+            fruits[v[left]]--;
+            if(fruits[v[left]] == 0) {
+                fruits.erase(v[left]);
+            }
+            left++;
+        }
+        else {
+            maxLen = max(maxLen, right - left + 1);
+        }
+        right++;
+    }
+    return maxLen;
+}
+
+// ----------------------------------------------------------------------- Problem 7 ---------------------------------------------------------------------------------------------------------------->
+// Problem : Longest sub string with at most K distinct characters.
+// Brute Force :
+// Optimal :
+
+// Brute Force :
+int longestSubstringWithKDistinct(string str, int k) {
+    if(str.empty()) return 0;
+    
+    int maxLen = 0;
+    
+    for(int i = 0; i < str.length(); i++) {
+        set<int> st;
+        for(int j = i; j < str.length(); j++) {
+            st.insert(str[j]);
+            
+            if(st.size() > k) {
+                break;
+            }
+            
+            maxLen = max(maxLen, j - i + 1);
+        }
+    }
+    return maxLen;
+}
+
+// Optimal :
+int longestSubstringWithKDistinct(string str, int k) {
+    if(str.empty()) return 0;
+    
+    int maxLen = 0;
+    map<char, int> hash;
+
+    int left = 0; int right = 0;
+    while(right < str.length()) {
+        hash[str[right]]++;
+        
+        while(hash.size() > k)  {
+            hash[str[left]]--;
+            if(hash[str[left]] == 0) hash.erase(str[left]);
+            left++;
+        }
+        
+        maxLen = max(maxLen, right - left + 1);
+        right++;
+    }
+    return maxLen;
+}
+
+// Optimal 2 :
+int longestSubstringWithKDistinct(string str, int k) {
+    if(str.empty()) return 0;
+
+    int maxLen = 0;
+    map<char, int> hash;
+    
+    int left = 0; int right = 0;
+    while(right < str.length()) {
+        hash[str[right]]++;
+        
+        if(hash.size() > k)  {
+            hash[str[left]]--;
+            if(hash[str[left]] == 0) hash.erase(str[left]);
+            left++;
+        }
+        else {
+            maxLen = max(maxLen, right - left + 1);
+        }
+        right++;
+    }
+    return maxLen;
+}
+
+// ----------------------------------------------------------------------- Problem 8 ---------------------------------------------------------------------------------------------------------------->
+// Problem : Number of Substrings Containing All Three Characters
+// Brute Force : Generate every possible substring, track its distinct characters using a set, and increment the count whenever the substring contains all three characters.
+// Better : Generate substrings from each starting index, and once all three characters are found, add all possible extensions (n-j) and move ahead.
+// Optimal : Track the latest occurrence of each required character in an array, whenever all are present, add the minimum index plus one.
+
+// Brute Force :
+int allThreeCharactersSubstr(string str) {
+    if(str.empty()) return 0;
+    
+    int count = 0;
+    
+    for(int i = 0; i < str.length(); i++) {
+        set<char> hash;
+        for(int j = i; j < str.length(); j++) {
+            hash.insert(str[j]);
+            if(hash.size() == 3) count++;
+        }
+    }
+    return count;
+}
+
+// Better :
+int allThreeCharactersSubstr(string str) {
+    if(str.empty()) return 0;
+    
+    int count = 0;
+    int n = str.length();
+    
+    for(int i = 0; i < str.length(); i++) {
+        set<int> hash;
+        for(int j = i; j < str.length(); j++) {
+            hash.insert(str[j]);
+            if(hash.size() == 3) {
+                count = count + (n-j);
+                break;
+            }
+        }
+    }
+    return count;
+}
+
+// Optimal (For Specific Characters) :
+int allThreeCharactersSubstr(string str) {
+    if(str.empty()) return 0;
+    
+    int count = 0;
+    map<char, int> hash = {
+        {'a', -1},
+        {'b', -1},
+        {'c', -1}
+    };
+    int left = 0; int right = 0;
+
+    while(right < str.length()) {
+        hash[str[right]] = right;
+        
+        if(hash['a'] >= 0 && hash['b'] >= 0 && hash['c'] >= 0) {
+            count = count + min(hash['a'], hash['b'], hash['c']) + 1;
+        }
+        right++;
+    }
+    return count;
+}
+
+// Optimal (General Case) :
+int allThreeCharactersSubstr(string str) {
+    if(str.empty()) return 0;
+    
+    int count = 0;
+    int hashArray[3] = {-1, -1, -1};
+    int left = 0; int right = 0;
+    
+    while(right < str.length()) {
+        hashArray[str[right] - 'a'] = right;
+        
+        if(hashArray[0] >= 0 && hashArray[1] >= 0 && hashArray[2] >= 0) {
+            count = count + min(hashArray[0], hashArray[1], hashArray[2]) + 1;
+        }
+        right++;
+    }
+    return count;
+}
+
+// ----------------------------------------------------------------------- Problem 9 ---------------------------------------------------------------------------------------------------------------->
